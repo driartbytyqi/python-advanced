53bfe09247c2d2bd5753bbfc2d1b3d324095be6f
driartbytyqi <driartbytyqii@gmail.com>
2025-10-28 20:45:46 +0100

    variable

---
+++ lesson 1/variables.py
@@ -0,0 +1,19 @@
+temperatura = 98.6
+
+name = "john"
+
+print(type(temperatura))
+print(type(name))
+age = 30
+print("age is :" ,age)
+
+x=5
+y=5
+print(result)
+
+age = age + 1
+print("age now is :" ,age)
+first_name=("johnm")
+last_name=("doe")
+full_name=first_name + " " + last_name
+print(full_name)
