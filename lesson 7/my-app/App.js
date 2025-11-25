043d1fbf26eece0f553dd7641f1227b735f7ebb1
driartbytyqi <driartbytyqii@gmail.com>
2025-11-25 20:40:03 +0100

    day 7

---
+++ lesson 7/my-app/App.js
@@ -0,0 +1,43 @@
+// App.js
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DriartiScreen from "./screens/Diartiscreen";
+
+import MainScreen from "./screens/MainScreen";
+
+import ListScreen from "./screens/ListScreen"; 
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+     <NavigationContainer>
+       <Stack.Navigator
+         initialRouteName="List"
+        screenOptions={{
+           title: "App",
+         }}
+       >
+         <Stack.Screen name="Main" component={MainScreen} />
+
+         <Stack.Screen name="List" component={ListScreen} />
+
+           <Stack.Screen name="Driart" component={DriartiScreen} />
+      </Stack.Navigator>
+
+      <StatusBar style="auto" />
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
